c760062a217c8058793c58f6299f3e05b1c591c8
Yakov Varnaev <yakov.varnaev@mail.ru>
2022-04-25 00:06:01 +0300

    minor clean up

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView, ListView
+from django.views.generic import ListView
 
 User = get_user_model()
 
